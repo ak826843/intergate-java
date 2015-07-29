f63d447a72ea9f4432b992ed37b08c0c667a1142
Patrick <hopa1012@hs-karlsruhe.de>
2015-07-29 12:19:58 +0200

    Added console input to set the database credentials in the first place

--- src/main/java/de/hska/intergate/saml/sql/SQLConnectionFactory.java
+++ src/main/java/de/hska/intergate/saml/sql/SQLConnectionFactory.java
@@ -3,18 +3,61 @@ package de.hska.intergate.saml.sql;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 import java.util.prefs.Preferences;
 
 public class SQLConnectionFactory {
 	static Preferences preferences = Preferences
 			.userNodeForPackage(SQLConnectionFactory.class);
 	
-	public static final String mysqlAddress = "jdbc:mysql://thesis.deltatm.org:3306/";
-	public static final String mysqlDatabase = "intergate";
+	public static final String mysqlConnector = "jdbc:mysql://";
 	
 	public static Connection create() {
+		String username = preferences.get("db_username", null);
+		String password =  preferences.get("db_password", null);
+		String database = preferences.get("db_database", null);
+		String address = preferences.get("db_address", null);
+		
+		if(username == null) {
+			Scanner input = new Scanner(System.in);
+			
+			System.out.print("Enter the name of the database user: ");
+			username = input.next();
+			preferences.put("db_username", username);
+			
+			input.close();
+		}
+		if (password == null) {
+			Scanner input = new Scanner(System.in);
+			
+			System.out.print("Enter the password of the database user: ");
+			password = input.next();
+			preferences.put("db_password", password);
+			
+			input.close();
+		}
+		if (database == null) {
+			Scanner input = new Scanner(System.in);
+			
+			System.out.print("Enter the name of the database: ");
+			database = input.next();
+			preferences.put("db_database", database);
+			
+			input.close();
+		}
+		if (address == null) {
+			Scanner input = new Scanner(System.in);
+			
+			System.out.print("Enter the address of the database ([sub.]domain.com[:port]): ");
+			address = input.next();
+			preferences.put("db_address", address);
+			
+			input.close();
+		}
+		
 		Connection connection = null;
-
+		String databaseURI = mysqlConnector + address + "/" + database;
+		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e1) {
@@ -23,8 +66,7 @@ public class SQLConnectionFactory {
 
 		try {
 
-			connection = DriverManager.getConnection(mysqlAddress
-					+ mysqlDatabase, preferences.get("db_username", null), preferences.get("db_password", null));
+			connection = DriverManager.getConnection(databaseURI,username,password);
 
 		} catch (SQLException e) {
 			System.out.println("Connection Failed! Check output console");
